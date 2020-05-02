76c09a88a9c88a0a70412d38d0b159e6d1713216
Alex Ines <lex@blackcat.watch>
2020-05-02 01:03:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(game_status)
+  puts " #{game_status[0]} | #{game_status[1]} | #{game_status[2]} "
+  puts "-----------"
+  puts " #{game_status[3]} | #{game_status[4]} | #{game_status[5]} "
+  puts "-----------"
+  puts " #{game_status[6]} | #{game_status[7]} | #{game_status[8]} "
+end
